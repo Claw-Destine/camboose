f92f4cbe8d27ff955c2be8d9ee253e7d10817b76
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-04-14 12:10:47 +0200

    Env is now loaded in one place

--- service/main.go
+++ service/main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 
 	"claw-destine.com/camboose/service/recipies"
+	"claw-destine.com/camboose/service/rest"
 )
 
+type appConfig struct {
+	Rest     rest.RestConfig       `envPrefix:"HTTP_"`
+	Recipies recipies.RecipeConfig `envPrefix:"RECIPIES_"`
+}
+
+type AppComponents struct {
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		slog.Info("Could not load .env. If the env variables are not provided by another mean it may result in a crash")
+	if err := godotenv.Load(); err != nil {
+		slog.Info("Could not load .env.", "error", err)
 	}
 
-	// parse
-	var cfg recipies.RecipeConfig
-	err = env.Parse(&cfg)
+	var cfg appConfig
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
 
-	// parse with generics
-	cfg, err = env.ParseAs[recipies.RecipeConfig]()
+	slog.Info("Recipe Path", "recipe-path", cfg.Recipies.RecipePath)
 
-	slog.Info("Recipe Path", "recipe-path", cfg.RecipePath)
+	rest.StartRestService(cfg.Rest)
 }
