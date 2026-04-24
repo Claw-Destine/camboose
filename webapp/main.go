6c8e7fb5b8ddcb2d4a017acdd5a5d6fd67eaf195
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-04-24 11:02:06 +0200

    HTMX swap in menu

--- webapp/main.go
+++ webapp/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"claw-destine.com/camboose/webapp/projects"
+	"claw-destine.com/camboose/webapp/tasks"
 	"github.com/a-h/templ"
 )
 
@@ -11,7 +12,8 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	http.Handle("/components", templ.Handler(projects.Projects()))
+	http.Handle("/components/projects", templ.Handler(projects.Projects()))
+	http.Handle("/components/tasks", templ.Handler(tasks.Tasks()))
 
 	http.ListenAndServe(":3000", nil)
 }
