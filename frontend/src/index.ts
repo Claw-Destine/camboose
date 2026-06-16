da6eb6625a59dbc34c58c3dcd452cc9b34730d1d
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2026-06-16 15:27:18 +0200

    Body component moved to lit

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,10 +1,8 @@
 import "bulma/css/bulma.min.css";
-import { registerCambBody } from "./components/body/body";
 import { registerNewProjectModal, registerProjectComponent, registerProjectsComponent } from "./components/projects/projects";
 import { registerEditVersionModal, registerSpecsComponent, registerVersionItem } from "./components/specs/specs";
-import "./components/body"
+import "./components/body/body"
 
-// registerCambBody();
 registerProjectComponent();
 registerProjectsComponent();
 registerNewProjectModal();
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -16,3 +16,9 @@ export function removeElement(query: string, root: Element) {
     const el = root.querySelector(query) as Element;
     el.remove()
 }
+export function getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+    return null;
+}
